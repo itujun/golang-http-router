65ab84cf1150dbde2e8c02989355257d08bab84a
Juna <itujun7@gmail.com>
2025-07-28 11:29:27 +0700

    params

---
+++ params_test.go
@@ -0,0 +1,27 @@
+package golanghttprouter
+
+import (
+	"fmt"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/julienschmidt/httprouter"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestParams(t *testing.T) {
+	router := httprouter.New()
+	router.GET("/user/:name", func(w http.ResponseWriter, r *http.Request, params httprouter.Params) {
+		name := params.ByName("name")
+		fmt.Fprintf(w, "Hello %s!", name)
+	})
+
+	request, _ := http.NewRequest("GET", "http://localhost:3000/user/Lev", nil)
+	recorder := httptest.NewRecorder()
+
+	router.ServeHTTP(recorder, request)
+
+	expectedBody := "Hello Lev!"
+	assert.Equal(t, expectedBody, recorder.Body.String(), "Expected response body to match")
+}
